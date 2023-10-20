21bfb365382fa2c12cb25db728fc94d02f8e52b5
Jeff Rasley <jerasley@microsoft.com>
2023-10-20 10:15:47 -0700

    add setuptools and dskernels module

---
+++ dskernels/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+
+__version__ = "0.0.0"
+try:
+    from .version import __version__
+except ImportError:
+    pass
