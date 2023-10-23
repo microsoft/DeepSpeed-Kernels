3be0fb87aabdd60a2b371feff2269eaffd513dc2
Jeff Rasley <jerasley@microsoft.com>
2023-10-23 14:31:28 -0700

    ensure CC list is quoted

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 set -ex
 
-# ensure all submodules are present before build
-git submodule update --init --recursive
-
 # set CUDA_ARCH_LIST if desired, otherwise falls back to native
-CUDA_ARCH_LIST="86" python setup.py bdist_wheel
+CUDA_ARCH_LIST="80;86" python setup.py bdist_wheel
