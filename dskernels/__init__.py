48f2de2c6d8c8e15ea832dd86d5b7e39c5354a99
Jeff Rasley <jerasley@microsoft.com>
2023-10-23 10:26:55 -0700

    lib path api

--- dskernels/__init__.py
+++ dskernels/__init__.py
@@ -3,8 +3,14 @@
 
 # DeepSpeed Team
 
+import os
+
 __version__ = "0.0.0"
 try:
     from .version import __version__
 except ImportError:
     pass
+
+
+def library_path():
+    return os.path.dirname(__file__)
