f0ba1e4c633560f718112d993f454d4b73227e59
Jeff Rasley <jerasley@microsoft.com>
2023-10-24 11:26:01 -0700

    add cuda arch validation

--- builder/builder.py
+++ builder/builder.py
@@ -4,15 +4,46 @@
 # DeepSpeed Team
 
 import os
+import subprocess
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
-import subprocess
+from packaging import version as pkg_version
+
+
+def installed_cuda_version():
+    cuda_home = os.environ.get("CUDA_HOME", None)
+    if cuda_home is None:
+        import torch.utils.cpp_extension
+        cuda_home = torch.utils.cpp_extension.CUDA_HOME
+    assert cuda_home is not None, "CUDA_HOME does not exist, unable to compile CUDA op(s)"
+    output = subprocess.check_output([cuda_home + "/bin/nvcc", "-V"], universal_newlines=True)
+    output_split = output.split()
+    release_idx = output_split.index("release")
+    release = output_split[release_idx + 1].replace(',', '')
+    return pkg_version.parse(release)
+
+
+def validate_arch_list(cuda_arch_list: str):
+    cuda_version = installed_cuda_version()
+    cuda_arch_list = cuda_arch_list.split(';')
+    for arch in cuda_arch_list:
+        if arch == 'native':
+            continue
+        try:
+            arch = int(arch)
+        except ValueError:
+            raise ValueError(f"Invalid CUDA_ARCH_LIST: {cuda_arch_list}. "
+                             "CUDA_ARCH_LIST must be a list of integers or 'native'.")
+
+        if cuda_version < pkg_version.parse('12.0'):
+            assert arch < 90, f"Compute capability of {arch} is not supported for CUDA {cuda_version}"
+
+        assert arch >= 80, f"Compute capability less than 80 is not supported for DeepSpeed kernels"
 
 
 class CMakeExtension(Extension):
     def __init__(self, name, sources=[]):
         super().__init__(name, sources)
-
 
 
 class CMakeBuild(build_ext):
@@ -27,6 +58,7 @@ class CMakeBuild(build_ext):
 
         # Pass through CUDA_ARCH_LIST if defined, otherwise fall back to native
         cuda_arch_list = os.environ.get('CUDA_ARCH_LIST', 'native')
+        validate_arch_list(cuda_arch_list)
 
         # Destination path for final binaries
         abs_build_lib = os.path.join(os.path.abspath(self.build_lib), "dskernels")
