d64ceb2afd6c607ead7f4e77af8c4f30ad19b76e
Jeff Rasley <jerasley@microsoft.com>
2023-10-25 12:10:03 -0700

    fix submodules + add release script

---
+++ release/release.sh
@@ -0,0 +1,13 @@
+set -ex
+
+rm -rf dist
+
+ts=$(date +%s)
+DS_KERNELS_BUILD_STRING=".dev${ts}" CUDA_ARCH_LIST="80;86" python setup.py bdist_wheel
+
+# rename whl to ensure portability
+fname=$(ls dist)
+nname=$(echo $fname | sed 's/cp[0-9]\+-cp[0-9]\+/py3-none/' | sed 's/linux/manylinux1/')
+mv "dist/$fname" "dist/$nname"
+
+twine upload dist/*.whl
